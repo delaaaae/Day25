a6a8cefd6377e3575149d9066f0bf38228028a6f
unknown <delic.amir09@gmail.com>
2021-04-01 23:58:18 +0200

    Initial import

---
+++ ex04/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import GetRequestSetHeaders from './GetRequestSetHeader';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestSetHeaders />
+    </div>
+  );
+}
+
+export default App;
